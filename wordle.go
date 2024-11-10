5d5bf93658601660fb5e8c2ef760fd74b8809435
cqb13 <cqb13.main@gmail.com>
2024-11-10 15:26:59 -0500

    input

--- wordle.go
+++ wordle.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/term"
 	"math/rand"
 	"os"
 	"strings"
+	"unicode"
 )
 
 func main() {
@@ -20,9 +22,68 @@ func main() {
 		{" ", " ", " ", " ", " "},
 	}
 
-	//current_word := 0
-
 	print_board(board)
+
+	current_word := 1
+	current_letter := 1
+
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		fmt.Println("Error setting terminal to raw mode:", err)
+		return
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	var b []byte = make([]byte, 1)
+
+	for {
+
+		_, err := os.Stdin.Read(b)
+		if err != nil {
+			fmt.Println("Error reading from stdin:", err)
+			break
+		}
+
+		term.Restore(int(os.Stdin.Fd()), oldState)
+
+		if err != nil {
+			fmt.Println("Error reading from stdin:", err)
+			break
+		}
+
+		var r rune = rune(b[0])
+
+		if r == 127 {
+			if current_letter > 0 {
+				fmt.Println(current_letter)
+				board[current_word-1][current_letter-1] = " "
+				if current_letter != 1 {
+					current_letter--
+				}
+			}
+		} else if unicode.IsLetter(r) {
+			board[current_word-1][current_letter-1] = string(r)
+			if current_letter != 5 {
+				current_letter++
+			}
+		} else if r == 27 {
+			break
+		} else if r == 13 {
+			if board[current_word-1][4] != " " {
+				current_letter = 1
+				current_word++
+			}
+		}
+
+		clear_terminal()
+		print_board(board)
+
+		oldState, err = term.MakeRaw(int(os.Stdin.Fd()))
+		if err != nil {
+			fmt.Println("Error re-setting terminal to raw mode:", err)
+			return
+		}
+	}
 
 	fmt.Println(answer)
 }
@@ -35,6 +96,11 @@ func print_board(board [6][5]string) {
 		fmt.Println("|       |       |       |       |       |")
 	}
 	fmt.Println("+-------+-------+-------+-------+-------+")
+}
+
+func clear_terminal() {
+	esc := 27
+	fmt.Printf("%c[2J%c[1;1H", esc, esc)
 }
 
 func load_words() []string {
