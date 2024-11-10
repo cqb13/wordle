fd25ed5a7e021cbc6a89e329d8a08bbd2a148db8
cqb13 <cqb13.main@gmail.com>
2024-11-10 16:17:54 -0500

    board checking

--- wordle.go
+++ wordle.go
@@ -5,6 +5,7 @@ import (
 	"golang.org/x/term"
 	"math/rand"
 	"os"
+	"slices"
 	"strings"
 	"unicode"
 )
@@ -41,6 +42,7 @@ func main() {
 		}
 	}
 
+	clear_terminal()
 	print_board(board)
 
 	current_word := 1
@@ -56,7 +58,6 @@ func main() {
 	var b []byte = make([]byte, 1)
 
 	for {
-
 		_, err := os.Stdin.Read(b)
 		if err != nil {
 			fmt.Println("Error reading from stdin:", err)
@@ -68,22 +69,21 @@ func main() {
 		var r rune = rune(b[0])
 
 		if r == 127 { // Backspace Key
-			if current_letter > 0 {
-				fmt.Println(current_letter)
-				board[current_word-1][current_letter-1].Letter = " "
-				if current_letter != 1 {
-					current_letter--
-				}
+			board[current_word-1][current_letter-1].Letter = " "
+			current_letter--
+			if current_letter < 1 {
+				current_letter = 1
 			}
 		} else if unicode.IsLetter(r) { // Any Letter
 			board[current_word-1][current_letter-1].Letter = string(r)
-			if current_letter != 5 {
+			if current_letter < 5 {
 				current_letter++
 			}
 		} else if r == 27 { // Escape Key
 			break
 		} else if r == 13 { // Enter Key
 			if board[current_word-1][4].Letter != " " {
+				board[current_word-1] = check_accuracy(board[current_word-1], answer)
 				current_letter = 1
 				current_word++
 			}
@@ -102,14 +102,58 @@ func main() {
 	fmt.Println(answer)
 }
 
+func check_accuracy(row [5]Square, answer string) [5]Square {
+	answer_letters := strings.Split(answer, "")
+
+	for index, square := range row {
+		if square.Letter == answer_letters[index] {
+			row[index].Status = Correct
+			answer_letters[index] = "-"
+			continue
+		}
+	}
+
+	for index, square := range row {
+		if square.Status == Correct {
+			continue
+		}
+
+		if slices.Contains(answer_letters, square.Letter) {
+			for i, letter := range answer_letters {
+				if letter == square.Letter {
+					answer_letters[i] = "-"
+					row[index].Status = SemiCorrect
+					break
+				}
+			}
+		}
+	}
+
+	return row
+}
+
 func print_board(board [6][5]Square) {
 	for _, row := range board {
 		fmt.Println("+-------+-------+-------+-------+-------+")
 		fmt.Println("|       |       |       |       |       |")
-		fmt.Printf("|   %v   |   %v   |   %v   |   %v   |   %v   |\n", row[0].Letter, row[1].Letter, row[2].Letter, row[3].Letter, row[4].Letter)
+		fmt.Printf("|   %v   |   %v   |   %v   |   %v   |   %v   |\n", make_colored(row[0]), make_colored(row[1]), make_colored(row[2]), make_colored(row[3]), make_colored(row[4]))
 		fmt.Println("|       |       |       |       |       |")
 	}
 	fmt.Println("+-------+-------+-------+-------+-------+")
+}
+
+func make_colored(square Square) string {
+	var Green = "\033[32m"
+	var Yellow = "\033[33m"
+	var Reset = "\033[0m"
+
+	if square.Status == Correct {
+		return Green + square.Letter + Reset
+	} else if square.Status == SemiCorrect {
+		return Yellow + square.Letter + Reset
+	} else {
+		return square.Letter
+	}
 }
 
 func clear_terminal() {
