7d31e8f28e9590fd17871018d02a7bed24dc75cf
cqb13 <cqb13.main@gmail.com>
2024-11-10 14:14:19 -0500

    print board

--- wordle.go
+++ wordle.go
@@ -9,9 +9,32 @@ import (
 
 func main() {
 	words := load_words()
-	word := pickWord(words)
+	answer := pickWord(words)
 
-	fmt.Println(word)
+	board := [6][5]string{
+		{" ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " "},
+	}
+
+	//current_word := 0
+
+	print_board(board)
+
+	fmt.Println(answer)
+}
+
+func print_board(board [6][5]string) {
+	for _, row := range board {
+		fmt.Println("+-------+-------+-------+-------+-------+")
+		fmt.Println("|       |       |       |       |       |")
+		fmt.Printf("|   %v   |   %v   |   %v   |   %v   |   %v   |\n", row[0], row[1], row[2], row[3], row[4])
+		fmt.Println("|       |       |       |       |       |")
+	}
+	fmt.Println("+-------+-------+-------+-------+-------+")
 }
 
 func load_words() []string {
